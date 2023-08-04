e5c7ddf015090b3c05cafa174db680ae5d6f033d
Lakshay <lakshay21060@iiitd.ac.in>
2023-08-04 18:48:27 +0530

    updated rsa.cpp

--- rsa.cpp
+++ rsa.cpp
@@ -1,55 +1,72 @@
-#include<iostream>
-#include<vector>
-#include<cstdint>
-#include"64_bit_prime_number.h"
+#include <iostream>
+#include <vector>
+#include <cstdint>
+#include "prime_number.h"
 using namespace std;
 
-/*
-- choose two prime numbers say (p & q)
-- N (mod number) = p*q
-- phi(N) = number of numbers coprime with N
-	i.e. phi(N) = (p-1)*(q-1)
-- choose e (encryption key) should satisfy two conditions
-	- 1 < e < phi(N)
-	- coprime with N, and phi(N) 
-- choose d (decryption key) should satisfy this relation
-	d*e(mod phi(N)) = 1
+int main()
+{
+	// PROCEDURE
+	big_number p, q, N, phi, e, d;
+	int bit_size = 64, k = 2;
 
-- encryption key = (e, N)
-- decryption key = (d, N)
-- cypher text = message^e(mod N)
-- decrypted text = cypher^d(mod N)
-*/
+	// STEP 1
+	// choose two prime numbers say (p & q)
+	p = get_random_prime(bit_size);
+	cout << "p :: " << p << endl;
+	q = get_random_prime(bit_size);
+	// p and q should be different
+	while (p == q)
+	{
+		q = get_random_prime(bit_size);
+	}
+	cout << "q :: " << q << endl;
 
-vector<uint64_t> getPrimeNumberPair() {
-	uint64_t p = 2, q = 7;
-	return {p, q};
-}
+	// STEP 2
+	// find N i.e. p*q
+	// will be used to take modulus
+	N = p * q;
+	cout << "N :: " << N << endl;
 
-uint64_t getModulus(uint64_t p, uint64_t q) {
-	return multiply_and_modulus(p, q, );
-}
+	// STEP 3
+	// find phi i.e (p-1)*(q-1)
+	// phi :: number of numbers between 1(inclusive) -
+	// - and N(inclusive) that are co prime with N
+	phi = (p - 1) * (q - 1);
+	cout << "f :: " << phi << endl;
 
-uint64_t getPhi(uint64_t p, uint64_t q) {
-	return (p-1)*(q-1);
-}
+	// STEP 4
+	// find e :: makes encryption key along with N
+	// i.e. (e, N) collectively make encryption key or public key
+	// `e` should satisfy two conditions
+	// - 1 < e < phi
+	// - coprime with N & phi
+	// a random prime number with same BIT_SIZE as p & q -
+	// - and not equal to p and q will satisfy these conditions
+	e = get_random_prime(bit_size);
+	big_number flag;
+	flag = gcd(e, phi);
+	while (flag != 1)
+	{
+		e = get_random_prime(bit_size);
+		flag = gcd(e, phi);
+	}
+	cout << "e :: " << e << endl;
 
-uint64_t getEncryptionKey(uint64_t N, uint64_t phi) {
-	// for (ll i = 1; i < phi; ++i) {
-		
-	// }
-	return 0;
-}
+	// STEP 5
+	// find d :: makes decryption key along with N
+	// i.e. (d, N) collectively make decryption key or private key
+	// `d` should satify this relation :: (d*e)(mod phi) = 1
+	// or d = (1 + k * phi) / e ; where k is any random constant
+	d = (1 + k * phi) / e;
+	cout << "d :: " << d << endl;
 
-int main() {
-	// vector<ll> primeNumbers = getPrimeNumberPair();
-	// ll p = primeNumbers[0], q = primeNumbers[1];
-	// cout << p << " " << q << endl;
-	// ll N = getModulus(p, q);
-	// cout << N << endl;
-	// ll f = getPhi(p, q);
-	// cout << f << endl;
-	uint64_t prime = get_random_64_bit_prime();
-	cout << prime;
+	// STEP 6
+	// encrypt & decrypt message
+	big_number message, cypher_text, decyphered_text;
+	// store message as a number
+	message = 12;
+	cypher_text = power_modulus(message, e, N);
+	decyphered_text = power_modulus(cypher_text, d, N);
 	return 0;
 }
